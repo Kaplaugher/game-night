61596e1b70164b28c11bfeaa256e997bcb65658d
kyle <kaplaugher94@gmail.com>
2024-04-16 16:53:07 -0400

    add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
 import Navbar from "~/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,11 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-        <Navbar />
-        <main className="container mx-auto px-4">{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
+          <Navbar />
+          <main className="container mx-auto px-4">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
